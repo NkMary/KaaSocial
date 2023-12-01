8a13db18b86372ccda952ee5589a410ae75bc9cb
Nkechinyere Urum-Eke <nk@NkechinyeresMBP.lan>
2023-11-30 22:57:45 -0500

    added css to navbar icons

--- kaasocial/src/Components/Navbar/Navbar.js
+++ kaasocial/src/Components/Navbar/Navbar.js
@@ -1,24 +1,27 @@
 import React from 'react'
-import "./navbar.css"
-import searchIcon from "../Images/searchIcon.png"
+import "./navbar.css";
+import searchIcon from "../Images/searchIcon.png";
+import notificationIcon from"../Images/notificationIcon.png";
+import messageIcon from "../Images/messageIcon.png";
+import profileIcon from "../Images/profileIcon.png";
 
 export default function Navbar() {
   return (
     <div className='mainNavbar'>
-        <div>
+        <div className='LogoContainer'>
             <p>KaaSocial</p>
         </div>
         <div>
-            <div>
-                <img src={`${searchIcon}`} alt=""/> 
+            <div className='searchInputContainer'>
+                <img src={`${searchIcon}`} className="Icons" alt=""/> 
                 <input type="text" name="" id="" />
             </div>
         </div>
-        <div>
-            <img src="" alt=""/>
-            <img src="" alt=""/>
+        <div className='IconsContainer'>
+            <img src={`${notificationIcon}`} className="Icons" alt=""/> 
+            <img src={`${messageIcon}`} className="Icons" alt=""/> 
             <div>
-                <img src="" alt=""/>
+                <img src={`${profileIcon}`} className="ProfileImage" alt=""/> 
                 <p>Nk</p>
             </div>
         </div>
