f1eb83727631fdb288251e02726511996c943cb1
Nkechinyere Urum-Eke <nk@NkechinyeresMBP.lan>
2023-12-01 15:29:15 -0500

    added additional containers to the component

--- kaasocial/src/App.js
+++ kaasocial/src/App.js
@@ -1,10 +1,17 @@
 import './App.css'
+import Leftbar from './Components/LeftsideContainer/Leftbar';
+import Mainpost from './Components/MainPostContainer/MainPost';
 import Navbar from './Components/Navbar/Navbar';
+import Rightbar from './Components/RightsideContainer/Rightbar';
 import Home from './Pages/Home/Home';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Leftbar/>
+      <Mainpost/>
+      <Rightbar />
+
     </div>
   );
 }
