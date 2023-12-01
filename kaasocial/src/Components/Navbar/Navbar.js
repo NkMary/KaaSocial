1eb9ef2ae914939f52c41f889ff9e4c33affd1e4
Nkechinyere Urum-Eke <nk@NkechinyeresMBP.lan>
2023-11-30 22:29:18 -0500

    added search icon to nav

--- kaasocial/src/Components/Navbar/Navbar.js
+++ kaasocial/src/Components/Navbar/Navbar.js
@@ -1,8 +1,29 @@
 import React from 'react'
 import "./navbar.css"
+import searchIcon from "../Images/searchIcon.png"
 
 export default function Navbar() {
   return (
-    <div className='mainNavbar'>Navbar</div>
+    <div className='mainNavbar'>
+        <div>
+            <p>KaaSocial</p>
+        </div>
+        <div>
+            <div>
+                <img src={`${searchIcon}`} alt=""/> 
+                <input type="text" name="" id="" />
+            </div>
+        </div>
+        <div>
+            <img src="" alt=""/>
+            <img src="" alt=""/>
+            <div>
+                <img src="" alt=""/>
+                <p>Nk</p>
+            </div>
+        </div>
+    
+    
+    </div>
   )
 }
